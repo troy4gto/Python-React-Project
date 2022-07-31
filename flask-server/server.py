ed0ab3ea098f4f819e24cd498b466f76ae0dee71
troy4gto <troygto4@gmail.com>
2022-07-31 13:12:06 -0400

    youtube tutorial finished

--- flask-server/server.py
+++ flask-server/server.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 # Members API Route
 
 
-@app.route('/')
-@app.route('/members')
+@app.route("/members")
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 5001)
+if __name__ == "__main__":
+    app.run(debug=True)
